acba86038017fc10bfd3cacc0046b7d3b1c7fa80
Bang Cao <caothanhbang2002@gmail.com>
2022-11-02 16:24:53 -0400

    add tokenizer, indexer, pagerank components

---
+++ indexer.py
@@ -0,0 +1,184 @@
+import json, gzip, time
+
+class Posting(object):
+    def __init__(self, playId, sceneId, wordPos = None):
+        self.playId = playId
+        self.sceneId = sceneId
+        self.positions = list(wordPos)
+
+class PostingList(object):
+    def __init__(self):
+        self.postings = dict()
+
+    def insert(self, playId, sceneId, wordPos):
+        if sceneId not in self.postings:
+            self.postings[sceneId] = Posting(playId, sceneId, [wordPos])
+        else:
+            self.postings[sceneId].positions.append(wordPos)
+
+    def getPostingList(self):
+        return self.postings
+
+
+class Indexer(object):
+    def __init__(self, inputFile, outputFolder, queriesFile):
+        with gzip.open(inputFile, 'r') as f:
+            self.data = json.loads(f.read().decode('utf-8'))
+        
+        self.outputFolder = outputFolder
+        self.queriesFile = queriesFile
+        self.invertedList = dict()
+        self.sceneList = dict()
+        self.playList = dict()
+
+        self.createInvertedList()
+
+    def createInvertedList(self):
+        for idx, s in enumerate(self.data["corpus"]):
+            tokens = s["text"].split()
+
+            if s["sceneId"] not in self.sceneList:
+                self.sceneList[s["sceneId"]] = 0
+            if s["playId"] not in self.playList:
+                self.playList[s["playId"]] = 0
+            
+            self.sceneList[s["sceneId"]] += len(tokens)
+            self.playList[s["playId"]] += len(tokens)
+
+            for i, token in enumerate(tokens):
+                if token not in self.invertedList:
+                    self.invertedList[token] = PostingList()
+                self.invertedList[token].insert(s["playId"], s["sceneId"], i)
+                
+    def getAvgSceneLen(self):
+        count = 0
+        for k, v in self.sceneList.items():
+            count += v
+        return count / len(self.sceneList)
+
+    def getMinLenScene(self):
+        return min(self.sceneList, key = self.sceneList.get)
+
+    def getMaxLenScene(self):
+        return max(self.sceneList, key = self.sceneList.get)
+
+    def getMinPlayScene(self):
+        return min(self.playList, key = self.playList.get)
+
+    def getMaxPlayScene(self):
+        return max(self.playList, key = self.playList.get)
+
+    def getScenceContainsPhrase(self, phrase):
+        words = phrase.split()        
+        res = list(self.invertedList[words[0]].getPostingList().keys())
+
+        if len(res) == 0:
+            return res
+
+        for i in range(1, len(words), 1):
+            curWord = words[i]
+            prevWord = words[i - 1]
+            postings = self.invertedList[curWord].getPostingList()
+            prevWordPostings = self.invertedList[prevWord].getPostingList()
+            temp = []
+            for scene in res:
+                if scene in postings:
+                    check = 0
+                    for prev in prevWordPostings[scene].positions:
+                        if (prev + 1) in postings[scene].positions:
+                            check += 1
+                            break
+                    
+                    if check != 0:
+                        temp.append(scene)
+            
+            res = temp
+            
+        return res
+
+    def processAndQuery(self, phrases, isScene):
+        if isScene:
+            res = self.getScenceContainsPhrase(phrases[0].strip())
+            for i in range(1, len(phrases)):
+                if len(res) == 0:
+                    return res
+
+                temp = self.getScenceContainsPhrase(phrases[i].strip())
+                res = list(set(res) & set(temp)) # Get intersection of 2 list
+
+            return set(res)
+        else:
+            res = self.extractPlayListFromSceneList(self.getScenceContainsPhrase(phrases[0].strip()))
+
+            for i in range(1, len(phrases)):
+                if len(res) == 0:
+                    return res
+
+                temp = self.extractPlayListFromSceneList(self.getScenceContainsPhrase(phrases[i].strip()))
+                res = list(set(res) & set(temp)) # Get intersection of 2 list
+
+            return set(res)
+
+    def processOrQuery(self, phrases, isScene):
+        if isScene:
+            res = set(self.getScenceContainsPhrase(phrases[0].strip()))
+            for i in range(1, len(phrases)):
+                if len(res) == 0:
+                    return res
+
+                temp = self.getScenceContainsPhrase(phrases[i].strip())
+                res = set(list(res) + list(temp)) # Get intersection of 2 list
+
+            return set(res)
+        else:
+            res = set(self.extractPlayListFromSceneList(self.getScenceContainsPhrase(phrases[0].strip())))
+
+            for i in range(1, len(phrases)):
+                if len(res) == 0:
+                    return res
+
+                temp = self.extractPlayListFromSceneList(self.getScenceContainsPhrase(phrases[i].strip()))
+                res = set(list(res) + list(temp)) # Get intersection of 2 list
+
+            return set(res)
+
+    def extractPlayListFromSceneList(self, scenes):
+        res = []
+
+        for scene in scenes:
+            res.append(scene.split(":")[0])
+
+        return res
+
+    def processQuery(self, query):
+        queryName = query[0]
+        isScene = query[1] == 'scene'
+        if query[2].lower() == "and":
+            results = list(self.processAndQuery(query[3:], isScene))
+        elif query[2].lower() == "or":
+            results = list(self.processOrQuery(query[3:], isScene))
+
+        results.sort()
+
+        with open(self.outputFolder + queryName + ".txt", "w") as f:
+            for res in results:
+                f.write(res + "\n")
+
+        return
+
+    def startQuery(self):
+        with open(self.queriesFile, 'r') as f:
+            maxTime = 0
+            maxQuery = ""
+            for line in f:
+                l = line.split("\t")
+                print("Start query " + l[0] + ": ")
+                startTime = time.time()
+                self.processQuery(l)
+                endTime = time.time() - startTime
+                if endTime > maxTime:
+                    maxQuery = l[0]
+                    maxTime = endTime
+                print("Finish query: " + str(endTime))
+            # print("Longest query: ", maxQuery)
+        return 
---
+++ pagerank.py
@@ -0,0 +1,88 @@
+import gzip
+
+class PageRank:
+    def __init__(self, lb, tau, filename):
+        self.lb = lb
+        self.tau = tau
+        self.pages = set()
+        self.inlinks = dict()
+        self.outlinks = dict()
+        f = gzip.open(filename, "rb")
+        while True:
+            line = f.readline()
+            if line == b'':
+                break
+            link = str(line, "utf-8")
+            src, des = link.split()
+            self.pages.add(src)
+            self.pages.add(des)
+            if src not in self.outlinks:
+                self.outlinks[src] = set()
+            if des not in self.inlinks:
+                self.inlinks[des] = set()
+            self.outlinks[src].add(des)
+            self.inlinks[des].add(src)
+
+    def readInput(self, inp):
+        self.pages = set()
+        self.inlinks = dict()
+        self.outlinks = dict()
+        lines = inp.split("\n")
+        for line in lines:
+            link = str(line, "utf-8")
+            src, des = link.split()
+            self.pages.add(src)
+            self.pages.add(des)
+            if src not in self.outlinks:
+                self.outlinks[src] = set()
+            if des not in self.inlinks:
+                self.inlinks[des] = set()
+            self.outlinks[src].add(des)
+            self.inlinks[des].add(src)
+
+    def countInlinks(self):
+        lst = dict()
+        for page in self.pages:
+            if page not in self.inlinks:
+                lst[page] = 0
+            else:
+                lst[page] = len(self.inlinks[page])
+        return dict(sorted(lst.items(), key = lambda x : x[1], reverse = True))
+
+    def getPageRank(self):
+        currentPR, resultPR = dict(), dict()
+        P = len(self.pages)
+        delta_x = dict()
+        delta_y = float((1 - self.lb) / P)
+        count = 0
+        res = float(self.lb / P)
+        initialPR = float(1 / P)
+        for page in self.pages:
+            if len(self.outlinks.get(page, set())) > 0:
+                delta_x[page] = float((1 - self.lb) / len(self.outlinks[page]))
+            currentPR[page] = initialPR
+        
+        while True:
+            count += 1
+            for page in self.pages:
+                resultPR[page] = res
+            delta = 0
+            for page in self.pages:
+                if page not in self.outlinks:
+                    desLst = set()
+                else:
+                    desLst = self.outlinks[page]
+                if len(desLst) > 0:
+                    for q in desLst:
+                        resultPR[q] += delta_x[page] * currentPR[page]
+                else:
+                    delta += delta_y * currentPR[page]
+            check = True
+            for page, rank in resultPR.items():
+                resultPR[page] += delta
+                if check and abs(resultPR[page] - currentPR[page]) >= self.tau:
+                    check = False
+                currentPR[page] = resultPR[page]
+        
+            if check:
+                return dict(sorted(resultPR.items(), key = lambda x: x[1], reverse = True))
